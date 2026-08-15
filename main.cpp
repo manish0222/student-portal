7a55134cb980082d9cb501d794c40486031c4e40
manish0222 <manishgodbole022@gmail.com>
2026-08-15 10:44:10 +0530

    Implement book file loading

--- main.cpp
+++ main.cpp
@@ -1,6 +1,50 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
 
 using namespace std;
+
+struct Book {
+    int id;
+    string title;
+    string author;
+    string status;
+};
+
+vector<Book> loadBooks() {
+    vector<Book> books;
+    ifstream file("books.txt");
+
+    if (!file) {
+        cout << "Unable to open books.txt\n";
+        return books;
+    }
+
+    string line;
+
+    while (getline(file, line)) {
+        size_t first = line.find('|');
+        size_t second = line.find('|', first + 1);
+        size_t third = line.find('|', second + 1);
+
+        if (first == string::npos ||
+            second == string::npos ||
+            third == string::npos) {
+            continue;
+        }
+
+        Book book;
+        book.id = stoi(line.substr(0, first));
+        book.title = line.substr(first + 1, second - first - 1);
+        book.author = line.substr(second + 1, third - second - 1);
+        book.status = line.substr(third + 1);
+
+        books.push_back(book);
+    }
+
+    return books;
+}
 
 void displayMenu() {
     cout << "\n===== BOOK PORTAL =====\n";
@@ -9,10 +53,11 @@ void displayMenu() {
     cout << "3. Add Book\n";
     cout << "4. Borrow Book\n";
     cout << "5. Return Book\n";
-    cout << "6. Exit \n";
+    cout << "6. Exit\n";
 }
 
 int main() {
+    vector<Book> books = loadBooks();
     int choice;
 
     cout << "Welcome to the Book Portal!\n";
